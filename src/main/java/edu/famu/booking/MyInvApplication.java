70e53e88ba76ef8942568be22395d1eea3062778
kyB1001 <59378562+kyB1001@users.noreply.github.com>
2022-12-10 19:47:33 -0500

    update

--- src/main/java/edu/famu/booking/MyInvApplication.java
+++ src/main/java/edu/famu/booking/MyInvApplication.java
@@ -7,16 +7,15 @@ import org.parse4j.Parse;
 import org.parse4j.util.ParseRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class BookingApplication {
+public class MyInvApplication {
 	public static void main(String[] args) {
 		ParseRegistry.registerSubclass(Hotel.class);
 		ParseRegistry.registerSubclass(Room.class);
 		ParseRegistry.registerSubclass(User.class);
 		Dotenv dotenv = Dotenv.configure().filename("env").load();
 		Parse.initialize(dotenv.get("PARSE_APP_ID"), dotenv.get("PARSE_REST_ID"));
-		SpringApplication.run(BookingApplication.class, args);
+		SpringApplication.run(MyInvApplication.class, args);
 	}
 }
